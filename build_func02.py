71f4d403f99eda05683cb62c751d59fdf690f72f
muxiddin <muxiddintemirov@gmail.com>
2022-10-14 17:32:21 +0500

    02 bajarildi

--- build_func02.py
+++ build_func02.py
@@ -1,5 +1,4 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
-def main (a):
-    return round (a, 2)
-print (main(-2.55))
+answer=round(3*(7/5-9/4),2)
+print (answer)
