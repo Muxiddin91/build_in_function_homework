548c1309a16364dd335e2d3bb3e77542714f2980
muxiddin <muxiddintemirov@gmail.com>
2022-10-14 16:56:44 +0500

    020202

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,5 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+def main (a):
+    return round (a, 2)
+print (main(-2.55))
